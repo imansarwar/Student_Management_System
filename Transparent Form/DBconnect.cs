fd02e28cb97ea6542c3a82a7afa550651072db0b
imaan sarwar <imannaqvi2000@gmail.com>
2023-04-30 15:55:35 -0700

    SMS - CSharp

---
+++ Transparent Form/DBconnect.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Transparent_Form
+{
+    /*
+     * In this class Create the connection between application and mysql database
+     * we need to install xampp and mysql connector to this project
+     * we need to create the student database
+     */
+
+    //singleton pattern implementation (singleton class)
+    public sealed class DbConnectionSingleton
+    {
+        private DbConnectionSingleton() 
+        {
+            Console.WriteLine("Connection Instance created");
+
+        }
+        private static DbConnectionSingleton ConnectionInstance;
+        public static DbConnectionSingleton GetConnectionInstance()
+        {
+            if (ConnectionInstance == null)
+            {
+                ConnectionInstance = new DbConnectionSingleton();
+            }
+            return ConnectionInstance;
+        }
+    }
+
+
+    class DBconnect
+    {
+        //Singleton object creation to demostrate the singleton pattern
+        DbConnectionSingleton Connection1 = DbConnectionSingleton.GetConnectionInstance();
+        DbConnectionSingleton Connection2 = DbConnectionSingleton.GetConnectionInstance();
+
+        //to create original connection
+        MySqlConnection connect = new MySqlConnection("datasource=localhost;port=3306;username=root;database=student");
+
+        //to get connection
+        public MySqlConnection getconnection
+        {
+            get
+            {
+                return connect;
+            }
+        }
+
+        //create a function to Open conncetion
+        public void openConnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Closed)
+                connect.Open();
+        }
+
+        //Create a fuction to close connection
+        public void closeConnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+                connect.Close();
+        }
+    }
+}
---
+++ Transparent Form/PrintAndFormatFunctionality.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Bridge Pattern
+namespace Transparent_Form
+{
+    //Abstract Bridge Component
+    public interface Formatter
+    {
+        string format();
+    }
+
+    //concrete formatter
+    public class HtmlPrintFormatter : Formatter
+    {
+        public string format()
+        {
+            return ("This is HTML formatted List (HtmlPrintFormatter)");
+        }
+    }
+    //concrete formatter
+    public class PdfPrintFormatter : Formatter
+    {
+        public string format()
+        {
+            return ("This is PDF formatted List (PdfPrintFormatter)");
+        }
+    }
+
+    //Abstract Logic Class
+    public abstract class Printer
+    {
+        public Formatter _formatter;
+        public abstract string print(Formatter _formatter);
+    }
+
+    //concrete logic class
+    public class HtmlPrinter : Printer
+    {
+        public override string print(Formatter _formatter)
+        {
+            return (_formatter.format());
+        }
+    }
+
+    //concrete logic class
+    public class PdfPrinter : Printer
+    {
+        public override string print(Formatter _formatter)
+        {
+            return (_formatter.format());
+        }
+    }
+    //comment to run bridge pattern
+    class PrintAndFormatFunctionality
+    {
+        public PrintAndFormatFunctionality()
+        {
+            Console.WriteLine("PRINT");
+        }
+
+    
+    }
+
+    //uncomment to run bridge pattern
+    /*static class PrintAndFormatFunctionality
+    {
+
+        
+        
+        /*static void Main()
+        {
+            Formatter formatter = new HtmlPrintFormatter();
+            String resultList = new HtmlPrinter().print(formatter);
+            Console.WriteLine(resultList);
+            System.Console.ReadKey();
+        }
+    }*/
+
+    }
